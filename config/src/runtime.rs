6e7ee2320a2679774c83c6f09dff55e3d6548633
CGair23 <chengeair23@sjtu.edu.cn>
2022-02-15 11:47:14 +0800

    Add task type and id in runtime configuration

--- config/src/runtime.rs
+++ config/src/runtime.rs
@@ -6,6 +6,8 @@ use std::io::BufReader;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TfConfig {
+    pub task_type: String,
+    pub task_id: String,
     pub cluster: Woker,
     pub grpc_server: GrpcServer,
 }
@@ -39,10 +41,11 @@ mod tests {
     fn test_config_parse() {
         let path = "../config.json";
         let tf_conf = TfConfig::from_json(path).unwrap(); 
+        assert_eq!(tf_conf.task_id, "uuid-001");
         if let Some(sock) = tf_conf.cluster.worker.get(0) {
             assert_eq!(sock, "localhost:12345");    // (*s, "localhost:12345".to_string());
         }
         assert_eq!(tf_conf.grpc_server.ip, "127.0.0.1");
-        assert_eq!(tf_conf.grpc_server.port, 7777);
+        assert_eq!(tf_conf.grpc_server.port, 50051);
     }
 }
