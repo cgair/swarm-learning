fe9846495c00cd5be8de926f7afa6f6ed917a065
CGair23 <chengeair23@sjtu.edu.cn>
2022-03-04 13:58:15 +0800

    Add callback and encoder tests

--- swarm/model/tests/callback_test.py
+++ swarm/model/tests/callback_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import pytest
 import numpy as np
 # from swarmCBB import SwarmCallback
-from utils import number_of_digits_post_decimal, caclulate_factor, contruct_result_string, faltten
+from typing import List
+from utils import number_of_digits_post_decimal, caclulate_factor, split_list_by_n
 
 def test_number_of_digits_post_decimal():
     assert number_of_digits_post_decimal(-0.1234567) == 7
@@ -10,10 +10,11 @@ def test_number_of_digits_post_decimal():
 
 
 def test_caclulate_factor():
-    weight = np.array([-0.00725056, 0.0168993, 0.00075373, -0.00990144, 0.00284106, 0.01322524, -0.00417293, 0.00808944, -0.02160874, 0.00079403])
-    assert caclulate_factor(weight, (10,)) == 8
+    weights = np.array([-0.00725056, 0.0168993, 0.00075373, -0.00990144, 0.00284106, 0.01322524, -0.00417293, 0.00808944, -0.02160874, 0.00079403])
+    assert caclulate_factor(weights) == 8
 
 
-def test_contruct_result_string():
-    weight = np.array([ 0.09356794, 0.02917077, 0.06075062, 0.00063394])
-    assert contruct_result_string(weight, 8) == "9356794,2917077,6075062,63394"
+def test_split_list_by_n():
+    list_tmp = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    tmp = split_list_by_n(list_tmp, 4)
+    assert list(tmp) == [[1, 2, 3, 4], [5, 6, 7, 8], [9]]
