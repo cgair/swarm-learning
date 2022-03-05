1d7492b64dc46c624bea015c21674a2e39ac01f5
HaroldLee931 <jinbo.l931@gmail.com>
2022-03-05 17:42:37 +0800

    update thread version, event listen bug fix

--- swarm/model/utils/listen_eth_event.py
+++ swarm/model/utils/listen_eth_event.py
@@ -3,57 +3,95 @@ import json
 import time
 
 
-def log_loop(event_filter, poll_interval, features, flags):
-    count = 0
-    result = True
-    while count <= 10:  #TODO: 等待时长上限 如果需要外部控制的话 请把这里改成True
-        for event in event_filter.get_new_entries():
-            for i in range(len(features)):
-                print(json.loads(Web3.toJSON(event))["args"][features[i]])
-                print(type(json.loads(Web3.toJSON(event))["args"][features[i]]))
-                print(flags[i])
-                print(type(flags[i]))
-                if json.loads(Web3.toJSON(event))["args"][features[i]] == flags[i]:
-
-                    result = True
-                else:
-                    result = False
-            if result:
-                print("hello")
-                return result
-        count += 1
-        time.sleep(poll_interval)
-    return False
+#  TODO: __all__ = ['Listener.init_task_event', 'update_task_event', 'record_event', 'prepare_event']
+#  当合约更新时更新此字段
+contract_recorder_abi = '[{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"}],"name":"InitTaskDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"uint64","name":"layer","type":"uint64"},{"indexed":false,"internalType":"uint64","name":"w_or_b","type":"uint64"},{"indexed":false,"internalType":"uint128","name":"offset","type":"uint128"}],"name":"PrepareGiftDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"},{"indexed":false,"internalType":"uint64","name":"layer","type":"uint64"},{"indexed":false,"internalType":"uint64","name":"w_or_b","type":"uint64"},{"indexed":false,"internalType":"uint128","name":"offset","type":"uint128"}],"name":"RecordDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"},{"indexed":false,"internalType":"uint256","name":"enrollCount","type":"uint256"}],"name":"UpdateTaskDone","type":"event"},{"constant":false,"inputs":[],"name":"clean","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"offset","type":"uint128"}],"name":"getGift","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"int128[]","name":"","type":"int128[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"offset","type":"uint128"}],"name":"getTest1","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"int128[]","name":"","type":"int128[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getTest2","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint128","name":"epoch","type":"uint128"},{"internalType":"uint128","name":"batch","type":"uint128"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"factor","type":"uint128"},{"internalType":"uint128","name":"offset","type":"uint128"},{"internalType":"int128[]","name":"para","type":"int128[]"}],"name":"recordPara","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"reqNum","type":"uint256"}],"name":"taskHandler","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
 
 
 class Listener:
-    def __init__(self, rpc_addr, contract_addr, contract_raw_abi):
+    """
+    本类被用于创建监听事件的方法, 初始化时需要如下参数 \n
+    rpc地址, 实例化的Recorder.sol地址, 监听次数(默认 10), 监听间隔(默认2 单位 sec) \n
+    由此确保通过thread调用时可以安全退出
+    """
+    def __init__(self, rpc_addr, contract_addr, max_req_times=10, interval=2):
         self.web3 = Web3(HTTPProvider(rpc_addr))
         self.contract_addr = contract_addr
-        self.contract_abi = json.loads(contract_raw_abi)
+        self.contract_abi = json.loads(contract_recorder_abi)
         self.contract = self.web3.eth.contract(
             address=self.contract_addr, abi=self.contract_abi)
+        self.max_req_times = max_req_times
+        self.interval = interval
+
+    def __log_loop(self, event_filter, features, flags):
+        count = 0
+        result = True
+        while count < self.max_req_times:  # count <= 10:
+            for event in event_filter.get_new_entries():
+                count_pre_event = 0
+                for i in range(len(features)):
+                    ''' 测试函数
+                    print(json.loads(Web3.toJSON(event))["args"][features[i]])
+                    print(type(json.loads(Web3.toJSON(event))["args"][features[i]]))
+                    print(flags[i])
+                    print(type(flags[i]))
+                    '''
+                    if json.loads(Web3.toJSON(event))["args"][features[i]] == flags[i]:
+                        count_pre_event += 1
+                    else:
+                        result = False
+                        count_pre_event = 0
+                if count_pre_event == len(features):  # 在上面的for循环遍历掉所有feature且都为True时则返回
+                    return result
+            count += 1
+            time.sleep(self.interval)
+        return False
 
     def init_task_event(self, task_id, who):
+        """
+        Return true when task init succ \n
+        task_id: uint256 \n
+        who: string, who INITED this task, string of eth account address such like '0x0123456789abcdef0123456789abcdef01234567'
+        """
         features = ['taskID', 'who']
         flags = [task_id, who]
-        log_loop(self.contract.events.InitTaskDone.createFilter(fromBlock='latest'),
-                 2, features, flags)
+        return self.__log_loop(self.contract.events.InitTaskDone.createFilter(fromBlock='latest'),
+                               features, flags)
 
     def update_task_event(self, task_id, who):
+        """
+        Return true when new partner join exist task succ \n
+        task_id: uint256 \n
+        who: string, who UPDATED this task, eth account address
+        """
         features = ['taskID', 'who']
         flags = [task_id, who]
-        log_loop(self.contract.events.UpdateTaskDone.createFilter(fromBlock='latest'),
-                 2, features, flags)
+        return self.__log_loop(self.contract.events.UpdateTaskDone.createFilter(fromBlock='latest'),
+                               features, flags)
 
     def record_event(self, task_id, who, layer, w_or_b, offset):
+        """
+        Return true when upload parameters to the block_chain_network succ \n
+        task_id: uint256 \n
+        who: string, who UPLOADED parameters, eth account address \n
+        layer: uint the index of ml model layer \n
+        w_or_b: uint, the type(weight or bias) of parameters \n
+        offset: uint the offset in the matrix
+        """
         features = ['taskID', 'who', 'layer', 'w_or_b', 'offset']
         flags = [task_id, who, layer, w_or_b, offset]
-        log_loop(self.contract.events.RecordDone.createFilter(fromBlock='latest'),
-                 2, features, flags)
+        return self.__log_loop(self.contract.events.RecordDone.createFilter(fromBlock='latest'),
+                               features, flags)
 
-    def prepare_event(self, task_id, layer, w_or_b, offset):
+    def gift_prepare_event(self, task_id, layer, w_or_b, offset):
+        """
+        Return true when parameters merged successfully by the block_chain_network node \n
+        task_id: uint256 \n
+        layer: uint the index of ml model layer \n
+        w_or_b: uint, the type(weight or bias) of parameters \n
+        offset: uint the offset in the matrix
+        """
         features = ['taskID', 'layer', 'w_or_b', 'offset']
         flags = [task_id, layer, w_or_b, offset]
-        log_loop(self.contract.events.PrepareGiftDone.createFilter(fromBlock='latest'),
-                 2, features, flags)
+        return self.__log_loop(self.contract.events.PrepareGiftDone.createFilter(fromBlock='latest'),
+                               features, flags)
--- swarm/model/utils/listen_eth_tester.py
+++ swarm/model/utils/listen_eth_tester.py
@@ -1,19 +1,86 @@
 import listen_eth_event as lee
+import time
+import threading
 
 
+# 运行前请修改下面的东西
 rpcAddr = 'HTTP://127.0.0.1:7545'
-contractAddr = '0x64865fa8798AB182d90F29d1A6517aE815b6062F'
-contractRawABI = '[{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"}],"name":"InitTaskDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"uint64","name":"layer","type":"uint64"},{"indexed":false,"internalType":"uint64","name":"w_or_b","type":"uint64"},{"indexed":false,"internalType":"uint128","name":"offset","type":"uint128"}],"name":"PrepareGiftDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"},{"indexed":false,"internalType":"uint64","name":"layer","type":"uint64"},{"indexed":false,"internalType":"uint64","name":"w_or_b","type":"uint64"},{"indexed":false,"internalType":"uint128","name":"offset","type":"uint128"}],"name":"RecordDone","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"taskID","type":"uint256"},{"indexed":false,"internalType":"address","name":"who","type":"address"},{"indexed":false,"internalType":"uint256","name":"enrollCount","type":"uint256"}],"name":"UpdateTaskDone","type":"event"},{"constant":false,"inputs":[],"name":"clean","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"offset","type":"uint128"}],"name":"getGift","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"int128[]","name":"","type":"int128[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"offset","type":"uint128"}],"name":"getTest1","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"int128[]","name":"","type":"int128[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getTest2","outputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"taskID","type":"uint256"},{"internalType":"uint128","name":"epoch","type":"uint128"},{"internalType":"uint128","name":"batch","type":"uint128"},{"internalType":"uint64","name":"layer","type":"uint64"},{"internalType":"uint64","name":"w_or_b","type":"uint64"},{"internalType":"uint128","name":"factor","type":"uint128"},{"internalType":"uint128","name":"offset","type":"uint128"},{"internalType":"int128[]","name":"para","type":"int128[]"}],"name":"recordPara","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"uint256","name":"reqNum","type":"uint256"}],"name":"taskHandler","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
+contractAddr = '0xcc11Ba9e7bbD137c1535F7115Eb34755d636eAea'
+acc1 = '0x65968c459F61a5A332d3fb5C4c0Ae5aD18babbdE'  # 执行初始化 第一次上传的账户
+acc2 = '0x0C35a782c71ff9f927eaA1A16662107441f6Fa46'  # 执行加入任务 第二次上传的账户
+max_req_times = 60  # thread等待时间 超时自动退出
+interval = 2
+taskID = 1
 
-ll = lee.Listener(rpcAddr, contractAddr, contractRawABI)
-print(ll.contract_addr)
+ll = lee.Listener(rpcAddr, contractAddr, max_req_times, interval)
 
-# 这里换成自己的地址
-ll.init_task_event(12, '0x1147DDc032101bdd0d59D5dcB9aE513C880a6eB8')
-print("Done")
-ll.update_task_event(12, '0xF4a5746bC894780Bd2DC0C97498AFa77aC14aC19')
-print("Done")
-ll.record_event(12, '0xF4a5746bC894780Bd2DC0C97498AFa77aC14aC19', 1, 1, 1)
-print("Done")
-ll.prepare_event(12, 1, 1, 1)
-print("Finish")
+
+def th_1(account_address, ret):
+    if ll.init_task_event(taskID, account_address):
+        print("init_task_event succ")
+        ret.append(True)
+    else:
+        print("Related init_task_event not find in ", max_req_times, " times")
+        ret.append(False)
+    return
+
+
+def th_2(account_address, ret):
+    if ll.update_task_event(taskID, account_address):
+        print("update_task_event succ")
+        ret.append(True)
+    else:
+        print("Related update_task_event not find in ", max_req_times, " times")
+        ret.append(False)
+    return
+
+
+def th_3(account_address, ret):
+    if ll.record_event(taskID, account_address, 1, 1, 1):
+        print("record_event succ", account_address)
+        ret.append(True)
+    else:
+        print("Related record_event not find in ", max_req_times, " times")
+        ret.append(False)
+    return "custom_ret_value", 3
+
+
+def th_4(layer, w_or_b, offset, ret):
+    if ll.gift_prepare_event(taskID, layer, w_or_b, offset):
+        print("gift_prepare_event succ")
+        ret.append(True)
+    else:
+        print("Related gift_prepare_event not find in ", max_req_times, " times")
+        ret.append(False)
+    return "custom_ret_value", 4
+
+
+global thread_ret
+thread_ret = []
+
+start_time = time.time()
+threads = []  # 创建线程数组
+thread1 = threading.Thread(target=th_1, args=(acc1, thread_ret))
+# Init task 与 update task的人/账户不能是同一个
+thread2 = threading.Thread(target=th_2, args=(acc2, thread_ret))
+thread3_1 = threading.Thread(target=th_3, args=(acc1, thread_ret))
+# 下面这一行不会返回成功 因为测试case所需要的节点数是2 当节点数满足的时候会改为触发gift_prepare_event事件
+thread3_2 = threading.Thread(target=th_3, args=(acc2, thread_ret))
+thread4 = threading.Thread(target=th_4, args=(1, 1, 1, thread_ret))
+
+threads.append(thread1)  # 将线程1添加到线程数组
+threads.append(thread2)
+threads.append(thread3_1)
+threads.append(thread3_2)
+threads.append(thread4)
+
+for i in threads:
+    i.start()  # 启动线程
+
+for i in threads:
+    i.join()  #
+
+run_times = (time.time()-start_time)
+print('主线程和子线程运行时间共：%s' % run_times)
+print(thread_ret)
+
