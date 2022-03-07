12168f4693b0ddd0501cdff94db029b22cb7821f
CGair23 <chengeair23@sjtu.edu.cn>
2022-03-07 15:24:08 +0800

    Remove unsed function

--- swarm/model/utils/utils.py
+++ swarm/model/utils/utils.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 import os
-import subprocess
-import socket
-from time import sleep
+import sys
+import eth_utils
 from typing import List, Tuple
 from utils.weights_wrapper import Weights
+from utils.listen_eth_event import Listener
+from web3 import Web3
 
-# MODEL_DIR = "/swarm/model/checkpoints/"
-MODEL_DIR = "/swarm/swarm/model/checkpoints/"
-SPLIT_SIZE = 1024
 
-# The following item has been depreciated
-CLUSTER = ["61.129.70.134:50051", "61.129.70.134:50052"]
-UFS_CLI = "ufs"
-FS_ROOT_DIR = "/ufs/SMLNODE/fs/"
+MODEL_DIR = "/swarm/model/checkpoints/"
+SPLIT_SIZE = 4096
+
+DEFAULT_TX_GAS_PRICE = 80 * (10 ** 9)
+DEFAULT_TX_GAS = 2100000
 
 
 def file_prepared(epoch, batch):
@@ -22,52 +21,6 @@ def file_prepared(epoch, batch):
     if os.path.exists(model_path):
         return True, model_path
     return False, ""
-
-
-def send_file(task_id, filename):
-    # Here we simply use a loop to send insights to other workers in the cluster
-    for socket in CLUSTER:
-        ss = socket.split(":")
-        ip = ss[0]
-        port = int(ss[-1])
-        cmd = f"{UFS_CLI} upload --ip-address=\"{ip}\" -p {port} -f \"{filename}\" -t \"{task_id}\""
-        # Create a child process that does not wait
-        child = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env={'UFS_LOG': 'info'})    #将标准输出定向输出到subprocess.PIPE
-        child.wait() # 等待子进程结束
-        # print(child.stdout.read())
-        # print(child.stderr.read())
-        ret_bytes = child.stderr.read()
-        ret_string = str(ret_bytes, 'UTF-8')
-        print(ret_string)
-        if "OK" in ret_string:
-            return True
-        else:
-            return False
-
-
-def get_host_ip():
-    """
-    Query the local ip address
-    :return: ip
-    """
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    finally:
-        s.close()
-
-    return ip
-
-
-def get_merge_file(task_id, epoch, batch):
-    model_name = 'weights.{:0>2}-{:0>2}.h5'.format(epoch + 1, batch + 1)
-    peer_model_path = FS_ROOT_DIR + task_id + "/" + model_name
-    while True:
-        print("Searching file...")
-        sleep(5)
-        if os.path.exists(peer_model_path):
-            return peer_model_path
 
 
 def caclulate_factor(weights):
@@ -104,13 +57,60 @@ def split_list_by_n(list_collection: List[int], n=SPLIT_SIZE):
         yield list_collection[i: i + n]
 
 
-def send_msg(func: str, epoch: int, batch: int, layer: int, w_or_b: int, factor: int, offset: int, weight: List[int]):
+def processed(factor: int, slices) -> List[float]:
+    processed = [x/factor for x in slices]
+    return processed
+
+
+def send_msg(client, epoch: int, batch: int, layer: int, w_or_b: int, factor: int, offset: int, weight: List[int]):
     """
     """
+    addr = "0x19aeb665dfa6a8445a46cd9a5c666ac6c0d03c54"
+    priv_key = "0xb205017cc1b95e12aa37784b3e66eaf099ba6cf0e80cf10f8fc87b44abba53a7"
+
+    func_proto = eth_utils.keccak(b"recordPara(uint256,uint128,uint128,uint64,uint64,uint128,uint128,int128[])")
+    data_prefix = func_proto[0:4].hex()
     w = Weights(epoch, batch, layer, w_or_b, factor, offset)
     w.from_weights(weight)
     data_body = w.encode()
-    return data_body
+    data_hex = data_prefix + data_body
+    b_val = bytes.fromhex(data_hex)
+
+    nextNonce = client.get_nonce(addr)
+    print(f"[+] the next nonce of account {addr} is ", nextNonce)
+    epoch_number = client.epoch_number()
+    print("[+] epoch number is ", epoch_number)
+
+    # contract recorder addr = '0x8ad3bed567559a70d9c8d0dce08e0477b730f0d8'
+    tx = client.new_tx(sender=addr,
+                       receiver='0x8ad3bed567559a70d9c8d0dce08e0477b730f0d8', 
+                       nonce=nextNonce,
+                       gas_price=DEFAULT_TX_GAS_PRICE,
+                       gas=DEFAULT_TX_GAS,
+                       value=0, 
+                       data=b_val, 
+                       sign=True, 
+                       priv_key=priv_key, 
+                       epoch_height=epoch_number, 
+                       chain_id=0)
+    tx_hash = client.send_tx(tx)
+    sys.exit(f"[+] the send transaction hash is {tx_hash}") 
+
+    rpcAddr = 'http://192.168.1.21:12537'
+    contractAddr = '0x8723756d8b4cab11ffa39e316200513e32c8ef7c'
+    who = "0x19aeb665dfa6a8445a46cd9a5c666ac6c0d03c54" # account_address
+    max_req_times = 60  # After max_req_times, instance will give up on listen percific taskID and return false
+    interval = 2  # interval between req_time
+    taskID = 1
+    # When using python to connect to web3, the address needs to be checked first.
+    # for contract address we should accept lower case address because ganache, at the moment, print contract address in lower case.
+    # for account address we should accept only checksum address.
+    # ll = Listener(rpcAddr, Web3.toChecksumAddress(contractAddr), max_req_times, interval)
+    # # It will return true if init_task_event generated successfully
+    # print(ll.record_event(taskID, Web3.toChecksumAddress(who), layer, w_or_b, offset))
+    # while True:
+    #     if ll.record_event(taskID, Web3.toChecksumAddress(who), layer, w_or_b, offset):
+    #         return tx_hash
 
 
 def expand_with_factor(weights, factor: int) -> List[int]:
